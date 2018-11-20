be9befdfd365844913dcd84832a18b41655922fa
wolfrevokcats <boschierom.mb@gmail.com>
2018-11-20 11:21:17 +0100

    Project_2/File project_2_spade (first task)

--- Project_2/project_2_spade.py
+++ Project_2/project_2_spade.py
@@ -1,6 +1,12 @@
 import itertools
 from copy import deepcopy
+from collections import defaultdict
 
+
+# freq_dict: {max_freq:[item]}
+freq_dict = {}
+# support = {(item): (supp_pos, supp_neg)}
+supp_dict = {}
 class Dataset:
     """Utility class to manadirge a dataset stored in a external file."""
 
@@ -49,14 +55,9 @@ class Dataset:
                 d[symbol].append((counter, id))
         return d
 
-# freq_dict: {max_freq:[item]}
-freq_dict = {}
-# support = {(item): (supp_pos, supp_neg)}
-supp_dict = {}
-
 class SearchNode:
     # constructor: each node has a reference to
-    def __init__(self,name,node,dict_pos,dict_neg):
+    def __init__(self, name, node, dict_pos, dict_neg):
         self.name = name
         self.node = node
         self.dataset_pos = dict_pos
@@ -64,7 +65,7 @@ class SearchNode:
 
     def compute_support(self):
         # compute support of the nodes
-
+        print("----- Compute support-----")
         # 1) Get all the possible sibling of a node
         candidates_children = set(self.dataset_pos.keys()).union(set(self.dataset_neg.keys()))
         # 2) Compute support of (item+y), with y = each possible candidates children
@@ -83,13 +84,18 @@ class SearchNode:
                     neg_supp.add(occurr[0])
             # update the supp dictionary
             supp_dict[tuple(self.name + [item])] = (len(pos_supp),len(neg_supp))
+            print("supp_dict")
+            print(supp_dict)
             # update the freq_dict
-            self.update_freq_dict(item, len(pos_supp)+len(neg_supp), k)
+            total_supp = len(pos_supp)+len(neg_supp)
+            self.update_freq_dict(item, total_supp , k)
 
     def update_freq_dict(self, item, item_support, k):
+        print("---- Update Frequency ----")
+        print("Method called by item ", item)
         # self = node, item = name of the node
         # combined supp already present in the freq_dict, not max length
-        if item_support in freq_dict:
+        if item_support in freq_dict.keys():
             freq_dict[item_support].append(self.name + ([item]))
         # max length reached
         elif len(freq_dict) == k:
@@ -100,36 +106,84 @@ class SearchNode:
         # combined supp not present, not max length
         else:
             freq_dict[item_support] = [self.name + ([item])]
+        print("freq_dict")
+        print(freq_dict)
 
-    def gerenate_children(self):
+    def generate_children(self):
         # One node can generate its children
-
+        print("---- Generating Children ----")
         # 1) Check support of its children
-    #    self.compute_support()
+        self.compute_support()
         # 2) Update freq_dict --> done directly inside compute_support each time I had something
         # 3) Take all the frequent items in freq_dict
         possible_children = [items for freq, items in freq_dict.items()]
         # take just the names
-        possible_children = set([names[-1] for names in list(itertools.chain.from_iterable(possible_children))])
+        # 4) Generate all the combinations
+        possible_children = [names[-1] for names in list(itertools.chain.from_iterable(possible_children))]
+        possible_children = set(possible_children)
+        print("possible_children")
         print(possible_children)
         # take them just once
-        # 4) Generate all the combinations
-        #for child in possible_children:
-        # 5) Look if there is a projected database of child
+        print("here")
+        for search_element in possible_children:
+            print("here2")
+            if self.dataset_pos != {} and self.dataset_neg != {}:
+                print("here3")
+                new_dict_pos = self.project_dB(possible_children, search_element, self.dataset_pos)
+                new_dict_neg = self.project_dB(possible_children, search_element, self.dataset_neg)
+            elif self.dataset_pos == {} and self.dataset_neg != {}:
+                print("here4")
+                new_dict_pos = {}
+                new_dict_neg = self.project_dB(possible_children, search_element, self.dataset_neg)
+            elif self.dataset_pos != {} and self.dataset_neg == {}:
+                print("here5")
+                new_dict_pos = self.project_dB(possible_children, search_element, self.dataset_pos)
+                new_dict_neg = {}
+            else:
+                print("here6")
+                return
+            print("search node")
+            SearchNode(self.name + [search_element], search_element, new_dict_pos, new_dict_neg).generate_children()
 
-    def project_dB(self, freq_dict, item, dataset):
+    def project_dB(self, set_items, element, dataset):
+        print("---- Building Projected Database ----")
         # 1) Release a copy of the dataset
         temp_dict = deepcopy(dataset)
 
         # 2) Take just the items in dataset that are frequent (inside freq_dict)
-        temp_dict = {key:value for key,value in temp_dict.items() if key in freq_dict}
+        # temp_dict = {item: [list of occurrences]} just for items with
+        temp_dict = {key:value for key,value in temp_dict.items() if key in set_items}
         print("temp dict", temp_dict)
 
-        # 3) Get first occurrences
-        if item in temp_dict.keys():
-            self.get_first()
+        # 3)
+        print(" Pruning ")
+        prune_dataset = defaultdict(list)
+        if element in temp_dict.keys():
+            # Get first occurrences of element
+            first_occurr = self.get_first(element, dataset)
+            # insert in prune_dataset all the not first occurrences of element
+            prune_dataset = {element: list(set(dataset[element]).difference(set(list(first_occurr.items()))))}
+            # the possible candidates
+            possible_candidates = [x for x in temp_dict.keys() if x != element]
+            # possible_candidates = list(set_items)
+            print("poss cand", possible_candidates)
+            for other_items in possible_candidates:
+                for values in dataset[other_items]:
+                    trans, pos = values
+                    if trans in first_occurr:
+                        # if the trans is present: other_items comes after itemset
+                        if pos > first_occurr[trans]:
+                            # print("This occ ", (trans, first_occurr_POS[trans]), "before this", (trans, pos))
+                            if other_items not in prune_dataset:
+                                prune_dataset[other_items] = [(trans, pos)]
+                            else:
+                                prune_dataset[other_items].append((trans, pos))
+        for key in prune_dataset.keys():
+            return {item:occurrences for item,occurrences in prune_dataset.items() if prune_dataset[k] != []}
+        return {}
 
     def get_first(self, itemset, dataset):
+        print("--- Get first ---")
         first_occurr = {}
         for iter in range(len(dataset[itemset])):
             key = dataset[itemset][iter][0]
@@ -143,37 +197,38 @@ class SearchNode:
                     first_occurr[key] = dataset[itemset][iter][1]
         return first_occurr
 
-
 def sequence_mining(filepath_pos,filepath_neg,k):
     dict_pos = Dataset(filepath_pos).get_v()
     dict_neg = Dataset(filepath_neg).get_v()
-    print("Positive dataset")
-    print(dict_pos)
-    print("Negative dataset")
-    print(dict_neg)
+    #print("Positive dataset")
+    #print(dict_pos)
+    #print("Negative dataset")
+    #print(dict_neg)
 
 
-    myNode = SearchNode('A', '', dict_pos, dict_neg)
+    empty_node = SearchNode([], '', dict_pos, dict_neg)
+    empty_node.generate_children()
+
+def print_frequent(freq_dict, supp_dict):
+    print("Printing")
+    for kmost in freq_dict.values():
+        for value in kmost:
+            print(str(value), str(supp_dict[tuple(value)][0]), str(supp_dict[tuple(value)][1]),str(supp_dict[tuple(value)][0] + supp_dict[tuple(value)][1]))
 
 
-
-    """def print_frequent(freq_dict, supp_dict):
-        for kmost in freq_dict.values():
-            for value in kmost:
-                print(str(j), str(supp_dict[tuple(value)][0]), str(supp_dict[tuple(value)][1]),str(supp_dict[tuple(value)][0] + supp_dict[tuple(value)][1]))
-    print_frequent(freq_dict,supp_dict)"""
-    print(freq_dict)
-
-if __name__ == '__main__':
-    # Possible tests:
-    # prot1_PKA_group15.txt
-    # prot2_SRC1521.txt
-    # reu1_acq.txt
-    # reu2_earn.txt
+def main():
     global k
-    # sequence_mining("reu1_acq.txt","reu2_earn.txt",600)
-    k = 6
-    # sequence_mining("prot1_PKA_group15.txt","prot2_SRC1521.txt",5)
+    ##    pos_filepath = sys.argv[1] # filepath to positive class file
+    ##    neg_filepath = sys.argv[2] # filepath to negative class file
+    ##    k = int(sys.argv[3])
+    # TODO: read the dataset files and call your miner to print the top k itemsets
+    # test constnats
+    pos_filepath = 'positive.txt'
+    neg_filepath = 'negative.txt'
+    k = 1
+    if __name__ == "__main__":
+        sequence_mining(pos_filepath, neg_filepath, k)
+        print_frequent(freq_dict, supp_dict)
 
-    sequence_mining("positive.txt", "negative.txt",k)
 
+main()
--- Project_2/test.py
+++ Project_2/test.py
@@ -1,14 +1,14 @@
 import itertools
 from copy import deepcopy
 
-k = {1: ('A','B'), 2: ('C','D')}
-l = {7: ('A')}
-print(k)
+k = {7: [('A','B'),('C','E')], 2: ('C','D')}
+l = {7: ('Ck')}
+print(k.items())
 possible_children = [items for freq, items in k.items()]
 print(possible_children)
-possible_children = [i[-1] for i in list(itertools.chain.from_iterable(possible_children))]
+possible_children = [i for i in list(itertools.chain.from_iterable(possible_children))]
 print(possible_children)
 
 print(deepcopy(k))
-new_db = {r:v for r,v in k.items() if r in k}
+new_db = {r:v for r,v in k.items() if r in l}
 print("new",new_db)
